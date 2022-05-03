308847f3ddcafd02a617fa41b18dc505acd8f59e
rhean <rheandhont@gmail.com>
2022-05-03 17:39:26 +0200

    rewording

--- walp/scripts/image.py
+++ walp/scripts/image.py
@@ -5,23 +5,23 @@ from walp.utils import storage
 import os
 
 
-def importImage(rawpath):
-    coolpath = Path(rawpath)
-    if coolpath.is_dir():
+def importImage(raw_path):
+    cool_path = Path(raw_path)
+    if cool_path.is_dir():
         return 1
     else:
-        return handleimg(coolpath)
+        return handle_image(cool_path)
 
 
-def handleimg(path):
-    if checkisimage(path):
+def handle_image(path):
+    if check_is_image(path):
         storage.saveImage(path, os.path.basename(path))
         return 0
     else:
         return 1
 
 
-def checkisimage(path):
+def check_is_image(path):
     try:
         im = Image.open(path).copy()
         im.verify()
